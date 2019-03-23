65a30eb1e21814d77e4885e206acf5ac729c4a50
koty <kouichi.nishizawa+github@gmail.com>
2019-03-23 17:30:18 +0900

    カレンダーの表示まで実装。残りは地区選択の保存と毎朝の通知。

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'package:intl/intl.dart';
 
 class CalendarPage extends StatefulWidget {
   CalendarPage({Key key}) : super(key: key);
@@ -13,6 +14,7 @@ class CalendarItem {
   String date;
   String kind;
   bool not_available;
+  CalendarItem(this.date, this.kind, this.not_available);
 }
 
 class CalendarPageState extends State<CalendarPage> {
@@ -20,7 +22,7 @@ class CalendarPageState extends State<CalendarPage> {
 
   List<DropdownMenuItem<String>> _dropDownMenuItems;
   String _currentTown = "";
-  Map<String, List<CalendarItem>> _calendars = null;
+  Map<String, List<CalendarItem>> _calendars = new Map();
 
   @override
   void initState() {
@@ -32,7 +34,9 @@ class CalendarPageState extends State<CalendarPage> {
       });
     });
     getCalendar().then((val) {
-      _calendars = val;
+      setState(() {
+        _calendars = val;
+      });
     });
   }
 
@@ -44,8 +48,7 @@ class CalendarPageState extends State<CalendarPage> {
       var jsonResponse = convert.jsonDecode(responseBody);
       for (String key in jsonResponse.keys) {
         final value = jsonResponse[key];
-        // print('$key, $value');
-        menu.add(new DropdownMenuItem(value: value, child: Text("${key} ${value}")));
+        menu.add(new DropdownMenuItem(value: key, child: Text("${key} ${value}")));
       }
     } else {
       print("Request failed with status: ${response.statusCode}.");
@@ -55,11 +58,21 @@ class CalendarPageState extends State<CalendarPage> {
 
   Future<Map<String, List<CalendarItem>>> getCalendar() async {
     var response = await http.get("https://b-sw.co/nagano_gomi_calendar/gomi_calendar.json");
-    List<DropdownMenuItem<String>> menu = [];
     if (response.statusCode == 200) {
       String responseBody = convert.utf8.decode(response.bodyBytes);
        final jsonResponse = convert.jsonDecode(responseBody);
-       return jsonResponse;
+       final o = new Map<String, List<CalendarItem>>();
+       final dateFormatter = new DateFormat("yyyy-MM-dd");
+       final today = dateFormatter.format(DateTime.now());
+       for (var key in jsonResponse.keys) {
+         final List<CalendarItem> h = jsonResponse[key]
+             .map((x) => new CalendarItem(x['date'], x['kind'], x['not_available']))
+             .toList()
+             .cast<CalendarItem>();
+         h.sort((x, y) => x.date.compareTo(y.date));
+         o[key] = h.where((x) => today.compareTo(x.date) <= 0).toList();
+       }
+       return o;
     } else {
       print("Request failed with status: ${response.statusCode}.");
     }
@@ -69,17 +82,32 @@ class CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
     final List<Widget> items = [];
-    for (CalendarItem item in _calendars[_currentTown]) {
+    final calendar = _calendars.containsKey(_currentTown) ? _calendars[_currentTown] : [];
+    for (CalendarItem item in calendar) {
       items.add(new ListTile(title: new Text(item.date)));
     }
-    return Stack(
+
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         new DropdownButton(
           value: _currentTown,
           items: _dropDownMenuItems,
           onChanged: changedDropDownItem,
         ),
-        items,
+        new Expanded(
+          child: ListView.builder(
+            itemCount: calendar.length,
+            itemBuilder: (context, int index) {
+              final style = calendar[index].not_available == true
+                  ? new TextStyle(decoration: TextDecoration.lineThrough, fontSize: 26)
+                  : new TextStyle(fontSize: 26);
+              return Text(
+                "${calendar[index].date} ${calendar[index].kind}",
+                style: style,
+              );
+            }),
+        ),
       ],
     );
   }
